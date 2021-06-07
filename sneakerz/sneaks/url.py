ddedaa44f2c710554921bdb683cffe7f84d8583a
unknown <russellquaicoo1@gmail.com>
2021-06-07 13:00:37 +0000

    Static files have been added and templates have been set up. More to do.

--- sneakerz/sneaks/url.py
+++ sneakerz/sneaks/url.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
-    path('signup', views.signup, name="signup")
+    path('register/', views.register, name="register"),
+    path('signin/', views.signin, name="signin"),
+    path('store/', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout" )
 ]
--- sneakerz/sneaks/views.py
+++ sneakerz/sneaks/views.py
@@ -5,5 +5,20 @@ from django.http import HttpResponse, request
 def home(request):
     return render(request, 'home.html')
 
-def signup(request):
-    return render(request, 'signup.html')
+def register(request):
+    return render(request, 'register.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def store(request):
+    context = {} #for passing in data
+    return render(request, 'store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'cart.html', context)
+
+def checkout(request):
+    context = {} 
+    return render(request, 'checkout.html', context)
